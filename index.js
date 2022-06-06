b69f01ff335ae57fd885a7e992b37b79b6c1307a
Tomato6966 <chris.pre03@gmail.com>
2022-06-06 22:07:49 +0200

    Create index.js

---
+++ index.js
@@ -0,0 +1,9 @@
+//importing the files
+const remoteCacheClient = require("./lib/remoteCacheClient");
+const remoteCacheServer = require("./lib/remoteCacheServer");
+
+//export this Pacakge
+module.exports = {
+    remoteCacheClient,
+    remoteCacheServer
+}
